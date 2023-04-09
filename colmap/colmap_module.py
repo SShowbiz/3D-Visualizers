61b9b8d9bf39cbf9b1a15c3f07d9908337a322ad
SShowbiz <hwkim408@gmail.com>
2023-04-09 08:47:48 +0000

    chore: remove unnecessaries

--- colmap/colmap_module.py
+++ colmap/colmap_module.py
@@ -1,6 +1,6 @@
 import os
-from utils import create_database, feature_extraction, feature_matching, save_output_as_txt, sparse_reconstruction
-
+from utils import create_database, feature_extraction, feature_matching, save_output_as_txt, sparse_reconstruction, colmap_to_json
+from pathlib import Path
 
 class COLMAP:
     def __init__(self) -> None:
@@ -33,44 +33,7 @@ class COLMAP:
             output_path=f"{save_path}/txt",
             output_type="TXT"
         )
-
-# def main(args):
-#     # Initial or undistorted images
-#     image_path = "preprocessed/scene" if args.initial else "preprocessed/undistorted_scene"
-#     data_folder = "colmap/data" if args.initial else "colmap/data_undistort"
-
-#     os.makedirs(data_folder, exist_ok=True)
-
-#     # Create Database
-#     create_database(database_path=f"{data_folder}/database.db")
-
-#     # Feature Extraction
-#     feature_extraction(
-#         database_path=f"{data_folder}/database.db",
-#         image_path=image_path,
-#         single_camera=1,
-#         camera_model="OPENCV"
-#     )
-
-#     # Feature Matching
-#     feature_matching(database_path=f"{data_folder}/database.db")
-
-#     # # Sparse Reconstruction
-#     # sparse_reconstruction(
-#     #     database_path=f"{data_folder}/database.db",
-#     #     image_path=image_path,
-#     #     output_path=f"{data_folder}/sparse"
-#     # )
-
-#     # # Save output as txt file
-#     # save_output_as_txt(
-#     #     input_path=f"{data_folder}/sparse/0",
-#     #     output_path=f"{data_folder}/txt",
-#     #     output_type="TXT"
-#     # )
-
-# if __name__ == "__main__":
-#     if not (args.initial or args.undistorted):
-#         args.initial = True
-
-#     main(args)
+        colmap_to_json(
+            recon_dir=Path(f"{save_path}/sparse/0"),
+            output_path=Path(save_path),
+        )
